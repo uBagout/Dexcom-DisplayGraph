f757ce271845d2924698b4e29927ff39f9c31d3b
Bagout <59146220+uBagout@users.noreply.github.com>
2022-06-02 02:22:30 +0100

    matplotlib plot animation + environment variables
    it finally saves location on restart, was annoying to redrag it over to preferred spot everytime

--- app.py
+++ app.py
@@ -5,19 +5,72 @@ matplotlib.use("TkAgg")
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import time
 import threading
 from pydexcom import Dexcom
 
 import numpy as np
 import os
+import dotenv
+
+dotenv_file = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenv_file)
+
+API_USER = os.environ["API_USER"]
+API_PASSWORD = os.environ['API_PASSWORD']
+TIMEFRAME= int(os.environ['TIMEFRAME'])
+XPOS = int(os.environ['XPOS'])
+YPOS = int(os.environ['YPOS'])
 
 #non-diabetic / non-dexcom user clarification:
 #bg = blood glucose - measure of blood sugars that diabetics use to manage their condition
 #transmitter - the transmitter attached to the dexcom sensor which we stick on our skin
 #receiver - bluetooth receiver, referring to whichever device the user is using to receive transmitter data (either their phone or the official dexcom device)
 
-dexcom = Dexcom("", "", True) #insert username and password here
+dexcom = Dexcom(API_USER, API_PASSWORD, True) #insert username and password here
+
+f = plt.figure(figsize=(5,2), facecolor="white", alpha=0)
+a = f.add_subplot()
+
+def animate(i):
+    res = dexcom.get_glucose_readings(TIMEFRAME) 
+
+    
+    a.clear()
+
+    a.set_xlim(TIMEFRAME//12, 0.0)
+    a.axis('off')
+    a.hlines([4.0 , 12.0], 0, TIMEFRAME//12, colors= ['r', 'y'])
+    if res != None and len(res) != 0: #no data check
+        #we remake our x (time) and y (bg value) lists to plot each time we display the graph.
+        #could optimise this by: changing each x value by 5 minutes, in update(), and if exceeds TIMEFRAME, pop x and y rather than recalculate full list each time         
+        x = []
+        y = []
+        timelimit = datetime.datetime.now()
+            
+        #evaluate our timelimit to n HOURS before current time (there is a better way to do this before structuring the timelimit as a datetime)
+        if timelimit.hour-TIMEFRAME//60 < 0:
+            timelimit = timelimit.replace(day = timelimit.day-1)
+            timelimit = timelimit.replace(hour = timelimit.hour-TIMEFRAME//60+24)
+        else:
+            timelimit = timelimit.replace(hour = timelimit.hour-TIMEFRAME//60)
+
+        #take all times in res that convene to the selected TIMEFRAME   
+        #could find earliest corresponding indice that fits within the TIMEFRAME, but we still need to iterate over to assign x so is a minimal improvement
+        for r in res:
+             if r.time > timelimit:
+                delta = (r.time - timelimit) // datetime.timedelta(0, 300) #300s = 5 minute intervals
+                x.append(delta) 
+                y.append(r.mmol_l)
+        x.reverse()
+    
+        a.plot(x, y, color='gray', alpha=1.0)
+        #12 sets of 5 minute intervals in an hour
+    
+        # finish tkinter gui stuff, save graph location for next run
+        #figure out better graph transparency, currently there is a non transparent outline around the plot, but it works fine on white backgrounds (most browser webpages)
+
 
 class App(tk.Tk):
     def __init__(self, *args, **kwargs): #use to initialise settings
@@ -49,58 +102,6 @@ class App(tk.Tk):
         self.grip.bind("<B1-Motion>", self.move)
 
 
-    def update(self, a, res): #this should run every 5 minutes
-        last = time.time()
-        while True:
-            if time.time() - last >= 300: 
-                last = time.time()
-                newread = dexcom.get_current_glucose_reading()
-                #check invalid reading - signal loss - sensor not available
-                if newread != None:
-                    res.insert(0, newread)
-                
-                if res != None and len(res) > 288: #288 max number of readings in a day, no need to keep more in storage for plot
-                        res.pop()
-
-                self.display_graph(a, res)
-        
-    def display_graph(self, a, res, hours = 6): #make hours a setting to be changed
-        a.clear()
-        a.set_xlim(hours*12, 0.0)
-        a.axis('off')
-
-        if res != None and len(res) != 0: #no data check
-            #we remake our x (time) and y (bg value) lists to plot each time we display the graph.
-            #could optimise this by: changing each x value by 5 minutes, in update(), and if exceeds timeframe, pop x and y rather than recalculate full list each time
-
-                   
-            x = []
-            y = []
-            timelimit = datetime.datetime.now()
-            
-            #evaluate our timelimit to n hours before current time (there is a better way to do this before structuring the timelimit as a datetime)
-            if timelimit.hour-hours < 0:
-                timelimit = timelimit.replace(day = timelimit.day-1)
-                timelimit = timelimit.replace(hour = timelimit.hour-hours+24)
-            else:
-                timelimit = timelimit.replace(hour = timelimit.hour-hours)
-
-            #take all times in res that convene to the selected timeframe   
-            #could find earliest corresponding indice that fits within the timeframe, but we still need to iterate over to assign x so is a minimal improvement
-            for r in res:
-                if r.time > timelimit:
-                    delta = (r.time - timelimit) // datetime.timedelta(0, 300) #300s = 5 minute intervals
-                    x.append(delta) 
-                    y.append(r.mmol_l)
-            x.reverse()
-            
-            a.plot(x, y, color='gray', alpha=1.0)
-
-        a.hlines([4.0 , 12.0], 0, hours*12, ['r', 'y']) #12 sets of 5 minute intervals in an hour
-
-        # finish tkinter gui stuff, save graph location for next run, use env variables / text config for program settings
-        #figure out better graph transparency, currently there is a non transparent outline around the plot, but it works fine on white backgrounds (most browser webpages)
-
     def startpos(self, event):
         self.x = event.x
         self.y = event.y
@@ -113,6 +114,10 @@ class App(tk.Tk):
         x = (event.x_root - self.x + self.winfo_x() - self.grip.winfo_rootx())
         y = (event.y_root - self.y + self.winfo_y() - self.grip.winfo_rooty())
         self.geometry(f"+{x}+{y}")
+        
+        dotenv.set_key(dotenv_file, "XPOS", str(x))
+        dotenv.set_key(dotenv_file, "YPOS", str(y))
 
 app = App()
+ani = animation.FuncAnimation(f, animate, interval=300000)
 app.mainloop()
